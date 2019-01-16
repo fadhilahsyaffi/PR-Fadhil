d3eaaeaaa00b111db46554f2fd7ab7cd3867bc9c
Hanesty <hanestyastinov@gmail.com>
2019-01-16 08:55:12 +0700

    PR aka Pekerjaan Rumah Soal 04 & 05

---
+++ PRLogic/PR/PR01/soal04.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR01
+{
+    class soal04
+    {
+        public soal04()
+        {
+            Console.Write("Masukkan nilai n: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int b = 0; b < n; b++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    if (b == k || b + k == n - 1 || b == n / 2 || k == n / 2)
+                    {
+                        Console.Write("*  ");
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                    }
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine("Press Any Button");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PRLogic/PR/PR01/soal05.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR01
+{
+    class soal05
+    {
+        public soal05()
+        {
+            Console.Write("Masukkan nilai n: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int b = 0; b < n; b++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    if (b == k || b + k == n - 1 || b == 0 || k == 0 || b == n-1 || k == n-1)
+                    {
+                        Console.Write("*  ");
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("Press Any Button");
+            Console.ReadKey();
+        }
+    }
+}
